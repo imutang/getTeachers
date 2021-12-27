1adb218dfbd65a16af1af45df73d2d448a3f4359
imutang <1447640235@qq.com>
2021-12-27 09:02:15 +0800

    test0.1

---
+++ src/main/java/com/example/demo/controller/TeacherController.java
@@ -0,0 +1,9 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeacherController {
+
+}
